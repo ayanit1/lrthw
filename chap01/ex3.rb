d9525f23372d9e11e164c8c84131facc37df5871
Albert <ayanit1@googlemail.com>
2017-01-23 13:48:07 +0000

    Made changes to exercise 3

--- chap01/ex3.rb
+++ chap01/ex3.rb
@@ -3,16 +3,16 @@ puts "I will now count my chickens:"
 
 # uses string interpolation to do maths.
 # 30 divided by 6 add 25 which equals 30
-puts "Hens #{25. + 30 / 6}"
+puts "Hens #{25.0 + 30.0 / 6.0}"
 # 25 multiplied by 3 is 75, modulo 4 is 3. 100 minus 3 equals 97
-puts "Roosters #{100 - 25 * 3 % 4}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
 
 # prints the sting...
 puts "Now I will count the eggs:"
 
 # 4 modulo 2 and 1 divided by 4 both equal 0. 3 plus 2 plus 1 equals 6
 # 6 minus 5 plus 6 equals 7
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # prints string...
 puts "Is it true that 3 + 2 < 5 - 7?"
