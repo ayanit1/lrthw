620fc727ce6b65e3b70639500f568a12cd7d1652
Albert <ayanit1@googlemail.com>
2017-01-23 12:50:01 +0000

    Added exercise 1

---
+++ chap01/ex1.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "Said" do not touch this.'
+puts "This is an example of another line!"
+
+# octothorpe / hash allows line of code to be ignored in terminal
+# can be used to print just one line from the above by
+# placing it in front. 
