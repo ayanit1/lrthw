2f6c065f312654e34f019e1a5627c4fdb0b2ed42
Albert <ayanit1@googlemail.com>
2017-01-23 13:45:07 +0000

    Added exercise 3

---
+++ chap01/ex3.rb
@@ -0,0 +1,39 @@
+# prints string...
+puts "I will now count my chickens:"
+
+# uses string interpolation to do maths.
+# 30 divided by 6 add 25 which equals 30
+puts "Hens #{25. + 30 / 6}"
+# 25 multiplied by 3 is 75, modulo 4 is 3. 100 minus 3 equals 97
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# prints the sting...
+puts "Now I will count the eggs:"
+
+# 4 modulo 2 and 1 divided by 4 both equal 0. 3 plus 2 plus 1 equals 6
+# 6 minus 5 plus 6 equals 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints string...
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# is 3 plus 2 less than 5 minus 7. Returns boolean which is false
+puts 3 + 2 < 5 - 7
+
+# prints question, does maths 3 plus 2
+puts "What is 3 + 2? #{3 + 2}"
+# prints question, does maths 5 minus 7
+puts "What is 5 - 7? #{5 - 7}"
+
+# prints string...
+puts "Oh, that's why it's false."
+
+# prints string...
+puts "How about some more."
+
+# prints string, calculates if 5 is greater than minus 2, returns boolean
+puts "Is it greater? #{5 > -2}"
+# prints string, calaculates if 5 is greater than or equal to minus 2, returns boolean
+puts "Is it greater or equal? #{5 >= -2}"
+# prints string, calculates if 5 is less than or equal to minus 2, returns boolean
+puts "Is it less or equal? #{5 <= -2}"
