f6e795a07762b75a668636d23b5ce3444c37e79d
Albert <ayanit1@googlemail.com>
2017-01-24 14:44:20 +0000

    Changes to exercise 15

--- chap01/ex15.rb
+++ chap01/ex15.rb
@@ -1,17 +1,23 @@
-# takes one arguement from command line and assigns it to variable filename
+# User input must be done in commandline before opening program. Input is assigned to
+# variable filename. .first gets only one arguement
 filename = ARGV.first
-# assigns txt to the process of opening file object file name
+
+# Txt holds the value of a file object.
 txt = open(filename)
 
 puts "Here's your file #{filename}:"
-# calls method read to txt variable, this prints the contents of the file
+# Prints file to which is assigned to at the command line, uses function on
+# txt called read showing what the file contains with no parameters.
 print txt.read
 
-# prompts user input from terminal and assigns to variable
 print "Type the filename again: "
+# User input is assigned to variable file_again
 file_again = $stdin.gets.chomp
 
-# repeats same process as above
 txt_again = open(file_again)
 
+# Prints second user input in command line.
 print txt_again.read
+
+print txt.close
+print txt_again.close
